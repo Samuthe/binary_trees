cd037a832c44d6c8315a7e4b79088c132dafa61a
Samuthe <ekatisamuel@gmail.com>
2022-09-23 16:18:18 +0300

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,12 +13,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 binary_treet *new_node;
 new_node = malloc(sizeof(binary_tree_t))
 if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
+{
+return (NULL);
+}
+new_node->n = value;
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+return (new_node);
 }
